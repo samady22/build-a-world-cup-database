ec21e28be9dd9323f3470f74c711da72025cc95f
samady22 <hikmat.samady1@yahoo.com>
2024-02-24 06:58:46 +0000

    add comments to insert_data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -6,10 +6,16 @@
 #you can rebuild a database using this comman in where .sql is not in psql. 
 #psql -U postgres < worldcup.sql
 
-PSQL="psql -X --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
 echo "$($PSQL "TRUNCATE games, teams")"
 
-cat test_games.csv | while IFS="," read  YEAR ROUND WINNER OPPONENT W_GOAL O_GOAL 
+cat games.csv | while IFS="," read  YEAR ROUND WINNER OPPONENT W_GOAL O_GOAL 
 do 
   if [[ $YEAR != year ]]
   then
@@ -18,6 +24,7 @@ do
   #if not found
   if [[ -z $WINNER_ID ]] 
   then 
+  #insert winner team
   INSERT_TEAMS_RESULT=$($PSQL "insert into teams(name) values('$WINNER')")
   if [[ $INSERT_TEAMS_RESULT == "INSERT 0 1" ]]
   then
@@ -33,6 +40,7 @@ do
   #if not found
   if [[ -z $OPPONENT_ID ]]
   then
+  #insert oppoment team
   INSERT_TEAMS_RESULT="$($PSQL "insert into teams(name) values('$OPPONENT')")"
    if [[ $INSERT_TEAMS_RESULT == "INSERT 0 1" ]]
   then
@@ -42,21 +50,15 @@ do
   OPPONENT_ID=$($PSQL "select team_id from teams where name='$OPPONENT'")
   fi
 
-echo "HERE $WINNER_ID"
-echo "HERE oo $OPPONENT_ID"
-  # INSERT_GAMES_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values ($YEAR, $ROUND, $WINNER_ID, $OPPONENT_ID, $W_GOAL, $O_GOAL)")
-  # if [[ $INSERT_GAMES_RESULT == 'INSERT 0 1' ]]
-  # then 
-  # echo "Inserted into games, $YEAR : $ROUND : '$WINNER' : '$OPPONENT'"
-  # fi
+  #insert into games
+  INSERT_GAMES_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) values ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $W_GOAL, $O_GOAL)")
+  if [[ $INSERT_GAMES_RESULT == 'INSERT 0 1' ]]
+  then 
+  echo "Inserted into games, $YEAR : '$ROUND' : '$WINNER' : '$OPPONENT'"
+  fi
   fi
 done 
 
-if [[ $1 == "test" ]]
-then
-  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
-else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-fi
+
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
